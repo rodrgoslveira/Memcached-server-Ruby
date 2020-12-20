298b0269324ec5e67517dc59bc06d88b7f86210e
Rodrigo Silveira <rodrgoslveira@gmail.com>
2020-12-20 15:34:38 -0300

    first-sockets-implementation

--- lib/MemCachedClient.rb
+++ lib/MemCachedClient.rb
@@ -1,5 +1,30 @@
+require 'socket'
+
 class MemCacheClient
-  def initialize(argument)
-    @argument = argument
+  attr_accessor :port, :hostname, :clientSocket
+  def initialize(host,port)
+    @hostname = 'localhost'
+    @port = 2000
+    @clientSocket = TCPSocket.open(self.hostname, self.port)
+  end
+
+  def closeConnection
+    self.clientSocket.close
+  end
+
+  def storeMessage(mssg)
+    self.clientSocket.puts(mssg)
+  end
+
+  def openConnection
+    self.clientSocket = TCPSocket.open(self.hostname, self.port)
+  end
+  
+  def listen
+    puts "\n--------------------------------------SERVER MESSAGE\n"
+    while line = self.clientSocket.gets     # Read lines from the socket
+        puts line.chop
+     end
+    puts "\n----------------------------------------------------\n"
   end
 end
