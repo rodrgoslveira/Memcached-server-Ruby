cce53bdac9729ce7b5f99146a0750f5495172988
Rodrigo Silveira <rodrgoslveira@gmail.com>
2021-01-27 01:15:22 -0300

    hotfixes

--- bin/server.rb
+++ bin/server.rb
@@ -1,11 +1,11 @@
-require_relative '../lib/MemCached'
+require_relative '../lib/memcached'
 #Simple server running
 begin
   puts "MemCachedServer-[host port]"
   intro = $stdin.gets.chomp
-  host,port = intro.split(/ /)
-  server = MemCacheServer.new(host,port.to_i)
-  server.runServerToListen
+  host, port = intro.split(/ /)
+  server = MemCacheServer.new(host, port.to_i)
+  server.run_server_to_listen
 rescue Exception => e
   puts "ERROR"
   puts ":>MESSAGE: #{e.message}"
--- lib/MemCached.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'DataTypeModule'
-require_relative 'Cache'
-require_relative 'MemCachedClient'
-require_relative 'MemCachedServer'
-require_relative 'MemCachedTSC'
